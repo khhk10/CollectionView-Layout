37b346b2ebff1cd3a04cba406cbbfded855fad10
khhk10 <kohe.ikeda@gmail.com>
2019-08-30 23:54:07 +0900

    CustomLayout class
    prepare() method

--- CollectionView-Layout/CustomLayout.swift
+++ CollectionView-Layout/CustomLayout.swift
@@ -3,9 +3,23 @@ import UIKit
 // カスタムレイアウト
 class CustomLayout: UICollectionViewLayout {
     
+    var attributesArray = [UICollectionViewLayoutAttributes]() // レイアウト情報
+    
+    // レイアウト情報を計算
+    override func prepare() {
+        guard let collectionView = collectionView else { return }
+        for item in 0..<collectionView.numberOfItems(inSection: 0) {
+            let indexPath = IndexPath(item: item, section: 0)
+            // レイアウト情報
+            let attributes = UICollectionViewLayoutAttributes(forCellWith: indexPath)
+            attributesArray.append(attributes)
+        }
+    }
+    
     // セルのサイズ
     override var collectionViewContentSize: CGSize {
-        return CGSize(width: 100, height: 100)
+        guard let collectionView = collectionView else { return CGSize(width: 0, height: 0) }
+        return CGSize(width: collectionView.bounds.width, height: collectionView.bounds.height)
     }
     
     // レイアウトの設定
